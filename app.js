9ba2c2135059f3087914602f2b1b6a4964d24ae3
chhotu marandi <cmarandi29@gmail.com>
2023-03-27 23:38:20 +0530

    creating buttons functions

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 let second = 0;
 let miliSecond = 0;
 
+
+
 const seconds = document.getElementById("seconds");
 const miliSeconds = document.getElementById("mili-seconds");
+const buttons = document.querySelectorAll(".btn");
 // console.log(seconds)
 
+
+function startTimer() {
+
+}
+
+function stopTimer() {
+
+}
+
+function resetTimer () {
+  
+}
 
 function secondTimer(){
   setInterval(() => {
@@ -18,5 +33,16 @@ function miliSecondTimer (){
     miliSeconds.innerHTML = miliSecond;
     miliSecond++;
   }, 100);
+}
+
+if (miliSeconds.innerHTML == 100) {
+  secondTimer();
+}
+
+// miliSecondTimer();
+// secondTimer();
+
+for (let buttons of btn) {
+  console.log(btn)
 }
 
