5f486413258c961a8ea878f1b029488b75fca548
chhotu marandi <cmarandi29@gmail.com>
2023-04-17 14:37:04 +0530

    timer function is working

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ let isStop = true;
 const stopwatch = document.querySelector(".stopwatch");
 const startBtn = document.getElementById("start");
 const stopBtn = document.getElementById("stop");
+const resetBtn = document.getElementById("reset");
 
 
 let ms = 0;
-let s = 0;
-let m = 0;
+let sec = 0;
+let min = 0;
 
 
 
@@ -20,25 +21,49 @@ function startTimer () {
 
 
 function timer (){
+  
+  ms = parseInt(ms);
+  sec = parseInt(sec);
+  min = parseInt(min);
+
   if(isStop == false) {
     ms++;
-    setTimeout('timer()',1000)
+    
+
+    if(ms == 100) {
+      ms = 0;
+      sec++;
+    }
+
+    if(sec == 60) {
+      sec = 0;
+      min++;
+    }
+
+    if(ms<10){
+      ms = "0" + ms;
+    }
+
+    if(sec < 10){
+      sec = "0" + sec;
+    }
+
+
+    stopwatch.innerHTML = min + " : " + sec + " : " + ms;
+
+    setTimeout('timer()',10)
   }
 }
 
 const stopTimer = () => {
-  isStop == true;
+  isStop = true;
+}
+
+const resetTimer = () => {
+  stopwatch.innerHTML = 00;
 }
 
 
-// const timer = () => {
-//   if(isStop == false) {
-//     console.log("hi");
-//     ms++;
-//     timer.innerHTML = ms;
-//     setTimeout("timer()", 1000);
-//   }
-// }; 
-
 startBtn.addEventListener("click", startTimer);
 stopBtn.addEventListener("click", stopTimer);
+resetBtn.addEventListener("click", resetTimer);
